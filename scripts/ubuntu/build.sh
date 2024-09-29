4dfa85fbb294b9bbacac8a899f63806ffad672cf
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-09-29 06:20:21 +0000

    Scripts: Ubuntu: Fix deps and add future support for windows and linux

--- scripts/ubuntu/build.sh
+++ scripts/ubuntu/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Install dependencies
-sudo apt update
-sudo apt install -y build-essential cmake zlib1g -dev libusb-1.0-0-dev qtbase5-dev
+  # Install dependencies
+  sudo apt update
+  sudo apt install -y build-essential cmake zlib1g-dev libusb-1.0-0-dev qtbase5-dev
 
 # Clone the Heimdall repository
 #if [ ! -d "Heimdall" ]; then
@@ -15,8 +15,20 @@ sudo apt install -y build-essential cmake zlib1g -dev libusb-1.0-0-dev qtbase5-d
 mkdir -p build
 cd build
 
-# Configure the build
+# Configure the build for Linux
 cmake -DCMAKE_BUILD_TYPE=Release ..
 
-# Build Heimdall
+# Build Heimdall for Linux
 make
+
+# Configure the build for Windows
+#cmake -DCMAKE_C_COMPILER=/usr/bin/x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-w64-mingw32-g++ ..
+
+# Build Heimdall for Windows
+#make
+
+# Configure the build for macOS
+#cmake -DCMAKE_SYSTEM_NAME=Darwin ..
+
+# Build Heimdall for macOS
+#make
